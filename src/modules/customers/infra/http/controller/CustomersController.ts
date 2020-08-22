438a537f8797c3882a940d2cbb7bc25c3fe8c8d2
marinamsm <marinasouza.msm@gmail.com>
2020-08-22 18:05:34 -0300

    Users injection, controller and service. POST /customers is ready

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,11 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+    const customerCreationService = container.resolve(CreateCustomerService);
+
+    const newCustomer = await customerCreationService.execute({ name, email });
+
+    return response.status(200).json(newCustomer);
   }
 }
